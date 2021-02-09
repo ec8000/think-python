1043c6bea2d492722aba51df8bc5bbdcfb1e234d
Ed Chan <chan.edmu@gmail.com>
2021-02-08 22:47:43 -0800

    commit solutions

---
+++ ex3/ex3.1.py
@@ -0,0 +1,10 @@
+def right_justify(s):
+    """ Takes a string named s as a parameter and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
+    """
+    result = ' '
+    for i in range(70 - len(s)-1):
+        result += ' '
+    result = result + s
+    print(result)
+
+
---
+++ ex3/ex3.2.py
@@ -0,0 +1,17 @@
+def do_twice(f, v):
+    """Takes a function object as an argument and calls it twice."""
+    f(v)
+    f(v)
+
+def print_twice(bruce):
+    """Prints the value of the parameter twice."""
+    print(bruce)
+    print(bruce)
+
+def do_four(f, v):
+    """Takes a function object and a value and calls the function four times, passing the value as a parameter."""
+    do_twice(f, v)
+    do_twice(f, v)
+
+
+
